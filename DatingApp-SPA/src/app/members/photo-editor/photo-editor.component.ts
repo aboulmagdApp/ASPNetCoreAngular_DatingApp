989f580f1ec5c43d400c4c68e93473fd83ffc77e
aboulmagdApp <aboulmagd@live.com>
2019-07-10 10:35:40 +0300

    Adding the Set Main Photo functionality

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -3,6 +3,8 @@ import { FileUploader } from 'ng2-file-upload';
 import { Photo } from 'src/app/_models/photo';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -14,7 +16,9 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
-   constructor(private authService: AuthService) { }
+   constructor(private authService: AuthService,
+               private userService: UserService,
+               private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.initializeUploader();
@@ -49,5 +53,11 @@ export class PhotoEditorComponent implements OnInit {
       }
     };
   }
-
+  setMainPhoto(photo: Photo) {
+    this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
+      console.log('Successfully set to main');
+    }, error => {
+        this.alertify.error(error);
+    });
+  }
 }
